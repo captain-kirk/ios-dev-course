43fb30e12738c0d26b441695dbd1f8889d88492e
Alicia Kirkland <aliciamonet2694@gmail.com>
2018-12-11 08:36:23 -0800

    completed the I Am Rich project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Alicia Kirkland on 12/10/18.
+//  Copyright © 2018 akirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
