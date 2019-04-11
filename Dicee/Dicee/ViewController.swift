865151e1c4486cd4d43fd3a89d5504d82421e5ed
Alicia Kirkland <alicia.kirkland@moovel.com>
2019-04-10 18:44:00 -0700

    starting over

---
+++ Dicee/Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Alicia Kirkland on 1/7/19.
+//  Copyright © 2019 Alicia Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
