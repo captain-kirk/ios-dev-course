62a4b64dbf400b82da9ba791423079154eb04fc2
Alicia Kirkland <alicia.kirkland@moovel.com>
2019-04-27 19:16:38 -0700

    adding test app

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Alicia Kirkland on 4/27/19.
+//  Copyright © 2019 Alicia Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+    
+    @IBAction func signUpPressed(_ sender: Any) {
+    }
+}
+
